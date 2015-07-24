0113737a274db4c1ad6ee6c4e2c4f61a9f9ef2ea
DCTL2015 <peta.crowe@corelogic.com.au>
2015-07-24 11:52:40 +1000

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,47 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This assignment is to write a pair of functions that will cache the inverse of a matrix.
 
-## Write a short comment describing this function
+# The function below will create a "matrix" object that can cache its inverse.
 
-makeCacheMatrix <- function(x = matrix()) {
+> makeCacheMatrix<-function(x=matrix()){
++ inv<-NULL
++ set<-function(y){
++ x<<-y
++ inv<<-NULL
++ }
++ get<-function()x
++ setinverse<-function(inverse) inve<<- inverse
++ getinverse<-function()inv
++ list(set=set,get=get,setinverse=setinverse,getinverse=getinverse)
++ }
 
-}
+## This function will calculate the inverse of the "matrix" returned by using the above makeCacheMatrix function.
+## If the inverse has already been calculated (and the matrix has not changed), 
+## then the cachesolve should retrieve the inverse from the cache.
 
+> cacheSolve<-function(x,...){
++ inv<-x$getinverse()
++ if(!is.null(inv)){
++ message("grabbing cached data")
++ return(inv)
++ }
++ data<-x$get()
++ inv<-solve(data)
++ x$setinverse(inv)
++ inv
++ }
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+## Return a matrix that is the inverse of 'x'
+## TEST RUN
+> x=rbind(c(1,10),c(10,1))
+> m=makeCacheMatrix(x)
+> m$get()
+     [,1] [,2]
+[1,]    1   10
+[2,]   10    1
+## No cache
+> cacheSolve(m)
+            [,1]        [,2]
+[1,] -0.01010101  0.10101010
+[2,]  0.10101010 -0.01010101
+> 
+## Retrieving cache
